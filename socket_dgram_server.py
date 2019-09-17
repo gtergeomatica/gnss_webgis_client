ed7ae7c8cb9e788797afdc7283d45b66f3e5d2be
rmarzocchi84 <roberto.marzocchi@gter.it>
2019-09-17 16:12:40 +0200

    Socket multithread funzionante. Il problema che resta da capire è se c'è un modo per gestire 2 dati con lo stesso iP

---
+++ socket_dgram_server.py
@@ -0,0 +1,24 @@
+#.py
+import socket
+import sys
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 8081)
+print('starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
+
+while True:
+    print('\nwaiting to receive message')
+    data, address = sock.recvfrom(1024)
+
+    print('received {} bytes from {}'.format(
+        len(data), address))
+    print(data)
+
+    if data:
+        sent = sock.sendto(data, address)
+        print('sent {} bytes back to {}'.format(
+            sent, address))
